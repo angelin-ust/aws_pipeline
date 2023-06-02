7d933d8c4129ab7dd28ab7f211fd9c2dad455740
angelin-ust <angelinammu.alex@ust.com>
2023-06-02 23:08:26 +0530

    first commit

---
+++ src/main/java/com/ust/rest/controller/Connect.java
@@ -0,0 +1,17 @@
+package com.ust.rest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/check")
+public class Connect {
+
+	
+	
+	@GetMapping
+	public String ready() {
+		return "1.2 3....ready";
+	}
+}
